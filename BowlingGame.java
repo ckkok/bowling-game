8561eaa44e41ca51d26c1efeb755d9d34670a2d6
Kok Chee Kean <kok_chee_kean@tech.gov.sg>
2023-11-17 10:19:07 +0800

    init

--- BowlingGame.java
+++ BowlingGame.java
@@ -3,88 +3,88 @@ import java.util.Scanner;
 
 public class BowlingGame {
   public static void main(String... args) {
-    var player1Score = 0;
-    var player2Score = 0;
-    var round = 1;
-    var isLousyLane = true;
+    var s1 = 0;
+    var s2 = 0;
+    var rnd = 1;
+    var laneFlag = true;
     System.out.println("========================================");
     System.out.println("Welcome to Les Terrible Bowling Game!");
     System.out.println("Developed by: Kopi Pasta Studios Pte Ltd");
     System.out.println("========================================");
     var sc = new Scanner(System.in);
-    var random = new Random();
+    var rndm = new Random();
     while (true) {
-      System.out.println(String.format("Round %d", round));
+      System.out.println(String.format("Round %d", rnd));
       System.out.print("Player 1 - Enter a target integer between 1 to 10 (inclusive): ");
-      var player1TargetStr = sc.nextLine();
-      var player1Target = Integer.parseInt(player1TargetStr);
-      float deviation = 0.0f;
-      if (player1Target == 5) {
-        if (random.nextInt(2) == 0) {
-          if (!isLousyLane) {
-            deviation = random.nextFloat(-7.0f, 0.0f);
+      var t1Str = sc.nextLine();
+      var t1 = Integer.parseInt(t1Str);
+      float dev = 0.0f;
+      if (t1 == 5) {
+        if (rndm.nextInt(2) == 0) {
+          if (!laneFlag) {
+            dev = rndm.nextFloat(-7.0f, 0.0f);
           } else {
-            deviation = random.nextFloat(-10.0f, 1.0f);
+            dev = rndm.nextFloat(-10.0f, 1.0f);
           }
         } else {
-          if (!isLousyLane) {
-            deviation = random.nextFloat(0.0f, 7.0f);
+          if (!laneFlag) {
+            dev = rndm.nextFloat(0.0f, 7.0f);
           } else {
-            deviation = random.nextFloat(-1.0f, 10.0f);
+            dev = rndm.nextFloat(-1.0f, 10.0f);
           }
         }
-      } else if (player1Target > 5 && player1Target <= 10) {
-        deviation = random.nextFloat(-3.0f, 2.0f);
-      } else if (player1Target >= 0 && player1Target < 5) {
-        deviation = random.nextFloat(-2.0f, 3.0f);
+      } else if (t1 > 5 && t1 <= 10) {
+        dev = rndm.nextFloat(-3.0f, 2.0f);
+      } else if (t1 >= 0 && t1 < 5) {
+        dev = rndm.nextFloat(-2.0f, 3.0f);
       }
-      var actualTarget = Math.floor(player1Target + deviation);
+      var tgt = Math.floor(t1 + dev);
       var score = 0;
-      if (actualTarget <= 0) {
+      if (tgt <= 0) {
         score = 0;
-      } else if (actualTarget >= 11) {
+      } else if (tgt >= 11) {
         score = 0;
       } else {
-        score = 10 - Math.abs((int) actualTarget - 5);
+        score = 10 - Math.abs((int) tgt - 5);
       }
       System.out.println(String.format("Player 1 scores: %d", score));
-      player1Score = player1Score + score;
+      s1 = s1 + score;
       System.out.print("Player 2 - Enter a target integer between 1 to 10 (inclusive): ");
-      var player2TargetStr = sc.nextLine();
-      var player2Target = Integer.parseInt(player2TargetStr);
-      deviation = 0.0f;
-      if (player2Target == 5) {
-        if (random.nextInt(2) == 0) {
-          deviation = random.nextFloat(-7.0f, 0.0f);
+      var t2Str = sc.nextLine();
+      var t2 = Integer.parseInt(t2Str);
+      dev = 0.0f;
+      if (t2 == 5) {
+        if (rndm.nextInt(2) == 0) {
+          dev = rndm.nextFloat(-7.0f, 0.0f);
         } else {
-          deviation = random.nextFloat(0.0f, 7.0f);
+          dev = rndm.nextFloat(0.0f, 7.0f);
         }
-      } else if (player2Target > 5 && player2Target <= 10) {
-        deviation = random.nextFloat(-3.0f, 2.0f);
-      } else if (player2Target >= 0 && player2Target < 5) {
-        deviation = random.nextFloat(-2.0f, 3.0f);
+      } else if (t2 > 5 && t2 <= 10) {
+        dev = rndm.nextFloat(-3.0f, 2.0f);
+      } else if (t2 >= 0 && t2 < 5) {
+        dev = rndm.nextFloat(-2.0f, 3.0f);
       }
-      actualTarget = Math.floor(player2Target + deviation);
+      tgt = Math.floor(t2 + dev);
       score = 0;
-      if (actualTarget <= 0) {
+      if (tgt <= 0) {
         score = 0;
-      } else if (actualTarget >= 11) {
+      } else if (tgt >= 11) {
         score = 0;
       } else {
-        score = 10 - Math.abs((int) actualTarget - 5);
+        score = 10 - Math.abs((int) tgt - 5);
       }
       System.out.println(String.format("Player 2 scores: %d", score));
-      player2Score = player2Score + score;
-      round = round + 1;
-      if (round > 5) {
+      s2 = s2 + score;
+      rnd = rnd + 1;
+      if (rnd > 5) {
         break;
       }
     }
     System.out.println("Game over!");
-    if (player1Score > player2Score) {
-      System.out.println("Player 1 wins! Score %d to %d".formatted(player1Score, player2Score));
+    if (s1 > s2) {
+      System.out.println("Player 1 wins! Score %d to %d".formatted(s1, s2));
     } else {
-      System.out.println("Player 2 wins! Score %d to %d".formatted(player2Score, player1Score));
+      System.out.println("Player 2 wins! Score %d to %d".formatted(s2, s1));
     }
     sc.close();
   }
--- bowling_game.py
+++ bowling_game.py
@@ -1,4 +1,4 @@
-import random
+import random as rndm
 from math import floor
 
 print("========================================")
@@ -7,67 +7,67 @@ print("Developed by: Kopi Pasta Studios Pte Ltd")
 print("========================================")
 print()
 
-round = 1
+rnd = 1
 
-player_1_score = 0
-player_2_score = 0
+s1 = 0
+s2 = 0
 
-is_lousy_lane = True
+laneFlag = True
 
 while True:
-    print(f"Round {round}!")
-    player_1_target = input("Player 1 - Enter a target integer between 1 to 10 (inclusive): ")
-    player_1_target = int(player_1_target.lower().strip())
-    if player_1_target == 5:
-        if random.randint(1, 2) == 1:
-            if not is_lousy_lane:
-                deviation = random.gauss(-3.5, 3.5)
+    print(f"Round {rnd}!")
+    t1 = input("Player 1 - Enter a target integer between 1 to 10 (inclusive): ")
+    t1 = int(t1.lower().strip())
+    if t1 == 5:
+        if rndm.randint(1, 2) == 1:
+            if not laneFlag:
+                dev = rndm.gauss(-3.5, 3.5)
             else:
-                deviation = random.gauss(-4.5, 5.5)
+                dev = rndm.gauss(-4.5, 5.5)
         else:
-            if not is_lousy_lane:
-                deviation = random.gauss(3.5, 3.5)
+            if not laneFlag:
+                dev = rndm.gauss(3.5, 3.5)
             else:
-                deviation = random.gauss(4.5, 5.5)
-    elif player_1_target > 5 and player_1_target <= 10:
-        deviation = random.gauss(-0.5, 2.5)
-    elif player_1_target >= 0 and player_1_target < 5:
-        deviation = random.gauss(0.5, 2.5)
-    actual_target = floor(player_1_target + deviation)
-    if actual_target <= 0:
+                dev = rndm.gauss(4.5, 5.5)
+    elif t1 > 5 and t1 <= 10:
+        dev = rndm.gauss(-0.5, 2.5)
+    elif t1 >= 0 and t1 < 5:
+        dev = rndm.gauss(0.5, 2.5)
+    tgt = floor(t1 + dev)
+    if tgt <= 0:
         score = 0
-    elif actual_target >= 11:
+    elif tgt >= 11:
         score = 0
     else:
-        score = 10 - abs(actual_target - 5)
+        score = 10 - abs(tgt - 5)
     print(f"Player 1 scores: {score}")
-    player_1_score = player_1_score + score
-    player_2_target = input("Player 2 - Enter a target integer between 1 to 10 (inclusive): ")
-    player_2_target = int(player_2_target.lower().strip())
-    if player_2_target == 5:
-        if random.randint(1, 2) == 1:
-            deviation = random.gauss(-3.5, 3.5)
+    s1 = s1 + score
+    t2 = input("Player 2 - Enter a target integer between 1 to 10 (inclusive): ")
+    t2 = int(t2.lower().strip())
+    if t2 == 5:
+        if rndm.randint(1, 2) == 1:
+            dev = rndm.gauss(-3.5, 3.5)
         else:
-            deviation = random.gauss(3.5, 3.5)
-    elif player_2_target > 5 and player_2_target <= 10:
-        deviation = random.gauss(-0.5, 2.5)
-    elif player_2_target >= 0 and player_2_target < 5:
-        deviation = random.gauss(0.5, 2.5)
-    actual_target = floor(player_2_target + deviation)
-    if actual_target <= 0:
+            dev = rndm.gauss(3.5, 3.5)
+    elif t2 > 5 and t2 <= 10:
+        dev = rndm.gauss(-0.5, 2.5)
+    elif t2 >= 0 and t2 < 5:
+        dev = rndm.gauss(0.5, 2.5)
+    tgt = floor(t2 + dev)
+    if tgt <= 0:
         score = 0
-    elif actual_target >= 11:
+    elif tgt >= 11:
         score = 0
     else:
-        score = 10 - abs(actual_target - 5)
+        score = 10 - abs(tgt - 5)
     print(f"Player 2 scores: {score}")
-    player_2_score = player_2_score + score
-    round = round + 1
-    if round > 5:
+    s2 = s2 + score
+    rnd = rnd + 1
+    if rnd > 5:
         break
 
 print("Game over!")
-if player_1_score > player_2_score:
-    print(f"Player 1 wins! Score {player_1_score} to {player_2_score}")
+if s1 > s2:
+    print(f"Player 1 wins! Score {s1} to {s2}")
 else:
-    print(f"Player 2 wins! Score {player_2_score} to {player_1_score}")
+    print(f"Player 2 wins! Score {s2} to {s1}")
